ff343de8ad5aa497d5e356e523789c87efccf8ac
Yacine <benabdesselam.yacine@gmail.com>
2017-05-15 17:24:31 +0100

    Add config info

---
+++ config/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoose    = require('mongoose');
+var access_mongo = require('./mongo/db_mongo_access.json');
+
+var mongo_uri = "mongodb://"+access_mongo.DB_USER+":"+access_mongo.DB_PASSWORD+"@"+access_mongo.DB_HOST+"/"+access_mongo.DB_NAME;
+
+mongoose.connect(mongo_uri, function(err) {
+    if (err) throw err;
+});
+
+module.exports = {
+	mongoose: mongoose,
+};
